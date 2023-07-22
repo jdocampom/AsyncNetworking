52666d9fa7fd9b9f76e811c5a275f9114f60878d
Juan Diego Ocampo <gummy_pond0v@icloud.com>
2023-07-21 21:37:16 -0500

    Modified HTTPMethod.swift

--- Sources/AsyncNetworking/Utilities/HTTPMethod.swift
+++ Sources/AsyncNetworking/Utilities/HTTPMethod.swift
@@ -1,37 +1,45 @@
 //
-//  File.swift
-//  
+//  HTTPMethod.swift
+//  AsyncNetworking
 //
-//  Created by Juan Diego Ocampo on 2023-07-21.
+//  Created by Juan Diego Ocampo on 2023-07-22.
 //
 
 import Foundation
 
-@objc public enum HTTPMethod: Int {
+/// Represents the  HTTP methods used in RESTful web services that are supported by the `AsyncNetworking` framework.
+@objc enum HTTPMethod: Int, CaseIterable {
     
+    /// Represents the `GET` HTTP method.
     case get
     
-    case delete
-    
+    /// Represents the `POST` HTTP method.
     case post
     
+    /// Represents the `DELETE` HTTP method.
+    case delete
+    
+    /// Represents the `PATCH` HTTP method.
     case patch
     
+    /// Represents the `PUT` HTTP method.
     case put
     
-    var stringValue: String {
+    /// A `String` literal that matches the method name associated with the given `HTTPMethod` case.
+    public var stringValue: String {
         switch self {
         case .get:
             return "GET"
-        case .delete:
-            return "DELETE"
         case .post:
             return "POST"
+        case .delete:
+            return "DELETE"
         case .patch:
             return "PATCH"
         case .put:
             return "PUT"
         }
+        
     }
     
 }
