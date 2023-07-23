a670b694c3cd5bd15323df4703f8631852257637
Juan Diego Ocampo <gummy_pond0v@icloud.com>
2023-07-22 21:06:41 -0500

    Added test cases for PATCH HTTP method.

--- Tests/AsyncNetworkingTests/PATCH/PatchHTTPMethodTests.swift
+++ Tests/AsyncNetworkingTests/PATCH/PatchHTTPMethodTests.swift
@@ -1,5 +1,5 @@
 //
-//  PostHTTPMethodTests.swift
+//  PatchHTTPMethodTests.swift
 //  AsyncNetworking
 //
 //  Created by Juan Diego Ocampo on 2023-07-22.
@@ -9,24 +9,24 @@ import Foundation
 import XCTest
 @testable import AsyncNetworking
 
-/// A test case class for testing the `PUT` HTTP method using the `AsyncNetworkManager`.
+/// A test case class for testing the `PATCH` HTTP method using the `AsyncNetworkManager`.
 ///
-/// This test case class is a subclass of `AsyncNetworkManagerTestCase`, which provides the testing environment and instance of the `AsyncNetworkManager` with the `.testing` environment configuration. The `PostHTTPMethodTests` class focuses on testing the `PUT` HTTP method for specific endpoints using the `AsyncNetworkManager`.
+/// This test case class is a subclass of `AsyncNetworkManagerTestCase`, which provides the testing environment and instance of the `AsyncNetworkManager` with the `.testing` environment configuration. The `PatchHTTPMethodTests` class focuses on testing the `PATCH` HTTP method for specific endpoints using the `AsyncNetworkManager`.
 ///
-/// **Testing the PUT HTTP Method:**
+/// **Testing the PATCH HTTP Method:**
 ///
-/// The `put` HTTP method is used to update data on the server by modifying some resources. In this test case class, we define test methods to ensure that the `AsyncNetworkManager` correctly handles `PUT` requests, performs the network communication, and decodes the server response into the expected model objects.
+/// The `patch` HTTP method is used to update data on the server by modifying some resources. In this test case class, we define test methods to ensure that the `AsyncNetworkManager` correctly handles `PATCH` requests, performs the network communication, and decodes the server response into the expected model objects.
 ///
 /// **Usage:**
 ///
-/// To use this test case class, run the individual test methods or all test methods collectively to verify the correctness of the `PUT` HTTP method implementation in the `AsyncNetworkManager`. The test methods are automatically executed by the testing framework, and any failed assertions will be reported as test failures.
+/// To use this test case class, run the individual test methods or all test methods collectively to verify the correctness of the `PATCH` HTTP method implementation in the `AsyncNetworkManager`. The test methods are automatically executed by the testing framework, and any failed assertions will be reported as test failures.
 ///
 /// **Example:**
 ///
 /// ```swift
-/// class MyPutHTTPMethodTests: AsyncNetworkManagerTestCase {
+/// class MyPatchHTTPMethodTests: AsyncNetworkManagerTestCase {
 ///
-///     // Test method for the PUT HTTP method with a specific endpoint.
+///     // Test method for the PATCH HTTP method with a specific endpoint.
 ///     func testUpdateUser() async throws {
 ///         // Create an Encodable object to hold the data that will be attached to the request as the httpBody.
 ///         let httpBody = UserProfileBody(name: "morpheus", job: "leader")
@@ -51,11 +51,11 @@ import XCTest
 /// ```
 ///
 /// - Note: This test case class inherits the testing environment and `AsyncNetworkManager` instance from the `AsyncNetworkManagerTestCase` class. It is marked as `final`, meaning it cannot be subclassed further. However, you can create new test case classes based on this class and extend its functionality as needed.
-final class PutHTTPMethodTests: AsyncNetworkManagerTestCase {
+final class PatchHTTPMethodTests: AsyncNetworkManagerTestCase {
     
     /// Test method for handling the successful scenario of updating a user's profile.
     ///
-    /// This private test method demonstrates the process of updating a user's profile on the server. It makes an HTTP PUT request with the provided endpoint details, where the endpoint path is set to "/api/users/2" to update the profile of the user with ID 2.
+    /// This private test method demonstrates the process of updating a user's profile on the server. It makes an HTTP PATCH request with the provided endpoint details, where the endpoint path is set to "/api/users/2" to update the profile of the user with ID 2.
     ///
     /// The `Endpoint` struct is used to specify the endpoint details, such as the decoding type, path, HTTP method, headers, and the HTTP body containing the updated user profile information (`httpBody`).
     ///
@@ -63,7 +63,7 @@ final class PutHTTPMethodTests: AsyncNetworkManagerTestCase {
     ///
     /// The test method sets the appropriate encoding and decoding strategies for the HTTP request and response, such as `.useDefaultKeys` for key encoding/decoding, `.iso8601` for date encoding/decoding, and `.base64` for data encoding/decoding. This ensures that the data is properly encoded and decoded for the server communication.
     ///
-    /// The test method performs the HTTP PUT request and awaits the response. The server is expected to respond with the updated user profile, and the test method validates the response by checking the updated name and job.
+    /// The test method performs the HTTP PATCH request and awaits the response. The server is expected to respond with the updated user profile, and the test method validates the response by checking the updated name and job.
     ///
     /// The test method uses `XCTAssert` statements to verify that the response contains the updated name ("morpheus") and job ("zion resident") as expected.
     ///
@@ -77,7 +77,7 @@ final class PutHTTPMethodTests: AsyncNetworkManagerTestCase {
         let endpoint = Endpoint(
             decodeType: UserProfileResponse.self,
             path: "/api/users/2",
-            httpMethod: .put,
+            httpMethod: .patch,
             headers: [:],
             httpData: httpBody
         )
